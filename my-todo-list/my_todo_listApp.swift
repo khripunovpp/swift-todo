96515b12694088b0015556e0878ab654f938eec2
Pavel Khripunov <pavel.khripunov@it.adcombo.com>
2023-12-15 21:48:46 +0000

    Initial Commit

---
+++ my-todo-list/my_todo_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_todo_listApp.swift
+//  my-todo-list
+//
+//  Created by Khripunov Pavel on 15/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct my_todo_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
